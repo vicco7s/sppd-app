e70e027363ad293c0961a6e5e810ecb18a0ab35a
Dwi Vicco <50991950+vicco7s@users.noreply.github.com>
2026-03-12 17:19:38 +0800

    anality varcel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <Toaster position="top-center" />
         {children}
+        <Analytics />
       </body>
     </html>
   );
